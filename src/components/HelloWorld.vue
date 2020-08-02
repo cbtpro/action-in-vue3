989fd36a7c5405b15ed4d4757a1065facb90c347
PeterChen <cbtpro@gmail.com>
2020-08-02 10:24:43 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  background-color: #fff;
+  border: 1px solid rgb(0, 0, 0);
+  border-radius: 0.5rem;
+  padding: 4px;
+  margin: 4px;
+  color: #000;
+  font-size: 1.5rem;
+}
+</style>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
